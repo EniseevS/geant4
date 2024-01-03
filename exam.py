7f331df0ef61f458b15d9e5bbd64a04ba1b12522
EniseevS <151023307+EniseevS@users.noreply.github.com>
2024-01-04 00:46:31 +0800

    Решение экзаменационной задачи
      -exam: Demo-версия. Созданы два класса. Задача решена согласно условию.

--- exam.py
+++ exam.py
@@ -4,23 +4,146 @@ import sys
 from geant4_pybind import *
 import math 
 
-class X5DetectorConstruction(G4VUserDetectorConstruction):
-   """
-   Simple model: a sphere with water in the box with air.
-   """
+#Detector construction
+class ExamDetectorConstruction(G4VUserDetectorConstruction):
  
    def __init__(self):
-     super().__init__()
-     self.fScoringVolume = None
+      super().__init__()
+      self.fScoringVolume = None
  
    def Construct(self):
-     nist = G4NistManager.Instance()
+      nist = G4NistManager.Instance()
 
-     envelop_x = 60*cm
-     envelop_y = 60*cm
-     envelop_z = 60*cm
+      envelop_x = 60*cm
+      envelop_y = 60*cm
+      envelop_z = 60*cm
 
-     envelop_mat = nist.FindOrBuildMaterial("G4_AIR")
+      envelop_mat = nist.FindOrBuildMaterial("G4_AIR")
 
-#....Leg
-     
+      zTrans = G4Transform3D(G4RotationMatrix(), G4ThreeVector(0.1*envelop_x, 0, 0.05*envelop_z))
+
+#.....Leg
+      mat_leg = nist.FindOrBuildMaterial("G4_TISSUE_SOFT_ICRP")
+      
+#.....Prosthesis
+      mat_p = nist.FindOrBuyildMaterial("G4_Ti")
+
+#.....Check for Overlaps
+      checkOverlaps = True
+
+#.....World creating 
+      world_x = 0.5*envelop_x
+      world_y = 1*envelop_y
+      world_z = 0.5*envelop_z
+
+      sWorld = G4Box("World", 0.5*world_x, 0.5*world_y, 0.5*world_z)
+ 
+      lWorld = G4LogicalVolume(sWorld, envelop_mat, "World")
+ 
+      pWorld = G4PVPlacement(None, G4ThreeVector(), lWorld, "World", None, False, 0, checkOverlaps)
+
+#.....Geometry volume creating
+
+      sLeg = G4Tubs("Leg", 0, 0.3*envelop_x, 0.5*envelop_y, 2*math.pi, 2*math.pi)
+      sProsthesis = G4Tubs("Prosthesis", 0, 0.05*envelop_x, 0.5*envelop_y, 2*math.pi, 2*math.pi)
+      sCut = G4SubtractionSolid("Cut", sProsthesis, sLeg, zTrans)
+
+#.....Logical volume creating
+
+      lLeg = G4LogicalVolume(sLeg, mat_leg, "Leg")
+      lProsthesis = G4LogicalVolume(sProsthesis, mat_p, "Prosthesis")
+
+#.....Physical volume creating
+
+      G4PVPlacement(None, G4ThreeVector(), lLeg, "Leg", lWorld, True, 0, checkOverlaps)
+      G4PVPlacement(None, G4ThreeVector(0.1*envelop_x, 0, 0.05*envelop_z), lProsthesis, "Prosthesis", lLeg, True, 0, checkOverlaps)
+
+      self.fScoringVolume = lLeg
+
+      return pWorld
+#End of detector construction
+
+#..........
+
+#Primary Generator
+class XXPrimaryGeneratorAction(G4VUserPrimaryGeneratorAction):
+   def __init__(self):
+      super().__init__()
+      self.fEnvelopeBox = None
+      self.fParticleGun = G4ParticleGun(1)
+
+   particleTable = G4ParticleTable.GetParticleTable()
+   particle = particleTable.FindParticle("neutron")
+   self.fParticleGun.SetParticleDefinition(particle)
+   self.fParticleGun.SetParticleMomentumDirection(G4ThreeVector(0, 0, -1))
+   self.fParticleGun.SetParticleEnergy(10*MeV)
+
+def GeneratePrimaries(self, anEvent):
+   envSizeX = 30*cm
+   envSizeY = 60*cm
+   envSizeZ = 30*cm
+
+    if self.fEnvelopeBox == None:
+       envLV = G4LogicalVolumeStore.GetInstance().GetVolume("Envelope")
+       if envLV != None:
+          self.fEnvelopeBox = envLV.GetSolid()
+          
+       if self.fEnvelopeBox != None:
+          envSizeX = self.fEnvelopeBox.GetXHalfLength()*2
+          envSizeY = self.fEnvelopeBox.GetYHalfLength()*2
+          envSizeZ = self.fEnvelopeBox.GetYHalfLength()*2
+       else:
+          msg = "Envelope volume of box shape not found.\n"
+          msg += "Perhaps you have changed geometry.\n"
+          msg += "The gun will be place at the center."
+          G4Exception("XXPrimaryGeneratorAction::GeneratePrimaries()", "MyCode0002", G4ExceptionSeverity.JustWarning, msg)
+
+       size = 0.5
+       x0 = 0 #size * envSizeX # * (G4UniformRand() - 0.5)
+       y0 = 0 # size * envSizeY * (G4UniformRand() - 0.5)
+       z0 = size * envSizeZ
+
+       self.fParticleGun.SetParticlePosition(G4ThreeVector(x0, y0, z0))
+       self.fParticleGun.GeneratePrimaryVertex(anEvent)
+#End of primary generator
+
+
+
+ui = None
+if len(sys.argv) == 1:
+  ui = G4UIExecutive(len(sys.argv), sys.argv)
+
+# Optionally: choose a different Random engine...
+# G4Random.setTheEngine(MTwistEngine())
+
+runManager = G4RunManagerFactory.CreateRunManager(G4RunManagerType.Serial)
+
+runManager.SetUserInitialization(ExamDetectorConstruction())
+
+# Physics list
+physicsList = QBBC()
+physicsList.SetVerboseLevel(1)
+
+runManager.SetUserInitialization(physicsList)
+
+# User action initialization
+#runManager.SetUserInitialization(ExamActionInitialization())
+
+visManager = G4VisExecutive()
+# G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+# visManager = G4VisExecutive("Quiet")
+visManager.Initialize()
+
+# Get the User Interface manager
+UImanager = G4UImanager.GetUIpointer()
+
+# # Process macro or start UI session
+if ui == None:
+  # batch mode
+  command = "/control/execute "
+  fileName = sys.argv[1]
+  UImanager.ApplyCommand(command + fileName)
+else:
+  # interactive mode
+  UImanager.ApplyCommand("/control/execute init_vis.mac")
+  ui.SessionStart()
